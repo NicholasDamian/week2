67b9868f631d466aab90c9193ec13613b5cc97bb
NicholasDamian <nicholashall@NicholasHallMBP.local>
2016-09-20 23:24:10 -0400

    Finished Tuesdays Homework

---
+++ day2/practice.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>List Favorite Cars</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Official Bootstrap -->
+    <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap-theme.css">
+    <!-- Our project CSS -->
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+
+<body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3"><img src="images/bakers.png" width="100%" alt="Bakers"></div>
+                <div class="col-sm-3"><img src="images/basil-hayden.png" width="100%" alt="Basil Hayden"></div>
+                <div class="col-sm-3"><img src="images/blantons.png" width="100%" alt="Blantons"></div>
+                <div class="col-sm-3"><img src="images/bookers.png" width="100%" alt="Bookers"></div>
+            </div>
+        </div>
+
+    <!-- Official JQuery -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <!-- Official Bootstrap -->
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+    <!-- Our project JS -->
+    <script src="bootstrap.js"></script>
+
+</body>
+
+</html>

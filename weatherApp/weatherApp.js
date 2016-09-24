cb598e6d0134bd657388ea856d1902d3340b7276
NicholasDamian <nicholashall@NicholasHallMBP.local>
2016-09-24 11:02:47 -0400

    Working on populating

--- weatherApp/weatherApp.js
+++ weatherApp/weatherApp.js
@@ -3,21 +3,14 @@ $(function () {
 
 
 
-    $("button#add").click(function () {
+    $("#add").click(function () {
         var zipCode = $("#enterzip").val();
-            $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + "&key=AIzaSyAw239Po20l0xNQoX6PW4yjvnRMB4ddJp8").done(function (data) {
+        $.ajax("https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode + "&key=AIzaSyAw239Po20l0xNQoX6PW4yjvnRMB4ddJp8").done(function (data) {
             var latitude = data.results[0].geometry.location.lat;
             var longitude = data.results[0].geometry.location.lng;
             var lngLat = latitude + "," + longitude;
             var state = data.results[0].address_components[2].short_name;
             var city = data.results[0].address_components[1].short_name;
-            $(".state").append("<span>" + state + "</span>");
-            $(".city").append("<span>" + city + "</span>");
-            getInfo(lngLat);
-            });
-        });
-    });
-    var getInfo = function(lngLat) {
             var darkSkyKey = "96f02ad95bc782facd7335190cb87a0a";
             $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + lngLat, { dataType: "jsonp" }).done(function (data) {
                 console.log(lngLat);
@@ -25,10 +18,30 @@ $(function () {
                 var rain = data.currently.precipProbability;
                 var min = data.daily.data[0].temperatureMin;
                 var max = data.daily.data[0].temperatureMax;
+                $("#populate").prepend("<div class = 'col-sm-3'> <h1>banna</h1> </div>");
                 $(".temp").append("<span>" + temp + "</span>");
                 $(".rain").append("<span>" + rain + "</span>");
                 $(".min").append("<span>" + min + "</span>");
                 $(".max").append("<span>" + max + "</span>");
+                $(".state").append("<span>" + state + "</span>");
+                $(".city").append("<span>" + city + "</span>");
             });
-    }
-    
+        });
+    });
+});
+
+// I call this outside of the function so the information processes in order
+// var getInfo = function (lngLat) {
+//     var darkSkyKey = "96f02ad95bc782facd7335190cb87a0a";
+//     $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + lngLat, { dataType: "jsonp" }).done(function (data) {
+//         console.log(lngLat);
+//         var temp = data.currently.temperature;
+//         var rain = data.currently.precipProbability;
+//         var min = data.daily.data[0].temperatureMin;
+//         var max = data.daily.data[0].temperatureMax;
+//         $(".temp").append("<span>" + temp + "</span>");
+//         $(".rain").append("<span>" + rain + "</span>");
+//         $(".min").append("<span>" + min + "</span>");
+//         $(".max").append("<span>" + max + "</span>");
+//     });
+// }
